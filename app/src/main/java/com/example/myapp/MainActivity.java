f3030e42dc5f02eed04a2fdddbebaeb319de3014
burnt776 <burnt776@gmail.com>
2020-06-08 00:50:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.EditText;
+import static android.graphics.Color.BLUE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RelativeLayout mylayout = new RelativeLayout(this);
+        Button mybutton =  new Button(this);
+
+
+
+        mylayout.setBackgroundColor(BLUE);
+        mybutton.setBackgroundColor(Color.GREEN);
+        mybutton.setText("lets go");
+        RelativeLayout.LayoutParams btndetails =  new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        btndetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        btndetails.addRule(RelativeLayout.CENTER_VERTICAL);
+        mylayout.addView(mybutton,btndetails);
+
+        EditText ed = new EditText(this);
+        mybutton.setId(1);
+        ed.setId(2);
+
+
+        RelativeLayout.LayoutParams edetails =  new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        edetails.addRule(RelativeLayout.ABOVE,mybutton.getId());
+        edetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        edetails.setMargins(0,0,0,50);
+        mylayout.addView(ed,edetails);
+
+
+
+        setContentView(mylayout);
+    }
+}
